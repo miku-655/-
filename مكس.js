89dd24c6f0ca89672ec69b5e1765877501f2e9ea
miku-655 <156179530+miku-655@users.noreply.github.com>
2024-08-02 16:11:43 +0300

    Update

---
+++ مكس.js
@@ -0,0 +1,254 @@
+[
+    {
+        "question": "كت ويليام",
+        "response": "ويليام"
+    },
+    {
+        "question": "كت نامي",
+        "response": "نامي"
+    },
+    {
+        "question": "كت لوفي",
+        "response": "لوفي"
+    },
+    {
+        "question": "كت ايرين",
+        "response": "ايرين"
+    },
+    {
+        "question": "كت استا",
+        "response": "استا"
+    },
+    {
+        "question": "كت همتارو",
+        "response": "همتارو"
+    },
+    {
+        "question": "كت الويفيا",
+        "response": "الويفيا"
+    },
+    {
+        "question": "كت نويل",
+        "response": "نويل"
+    },
+    {
+        "question": "كت رايلي",
+        "response": "رايلي"
+    },
+    {
+        "question": "كت ال",
+        "response": "ال"
+    },
+    {
+        "question": "كت لاو",
+        "response": "لاو"
+    },
+    {
+        "question": "كت مادارا",
+        "response": "مادارا"
+    },
+    {
+        "question": "كت يوريتشي",
+        "response": "يوريتشي"
+    },
+    {
+        "question": "كت ايتاتشي",
+        "response": "ايتاتشي"
+    },
+    {
+        "question": "كت توبي",
+        "response": "توبي"
+    },
+    {
+        "question": "كت نوبارا",
+        "response": "نوبارا"
+    },
+    {
+        "question": "كت ليفاي",
+        "response": "ليفاي"
+    },
+    {
+        "question": "كت هاشيراما",
+        "response": "هاشيراما"
+    },
+    {
+        "question": "كت بوروتو",
+        "response": "بوروتو"
+    },
+    {
+        "question": "كت يوسوب",
+        "response": "يوسوب"
+    },
+    {
+        "question": "كت ريم",
+        "response": "ريم"
+    },
+    {
+        "question": "كت اوتشيها شيسوي",
+        "response": "اوتشيها شيسوي"
+    },
+    {
+        "question": "كت كيد",
+        "response": "كيد"
+    },
+    {
+        "question": "كت ميكاسا",
+        "response": "ميكاسا"
+    },
+    {
+        "question": "كت دانزو",
+        "response": "دانزو"
+    },
+    {
+        "question": "كت وينغ",
+        "response": "وينغ"
+    },
+    {
+        "question": "كت تينشيهان",
+        "response": "تينشيهان"
+    },
+    {
+        "question": "كت بيكوهان",
+        "response": "بيكوهان"
+    },
+    {
+        "question": "كت كازيكاجي",
+        "response": "كازيكاجي"
+    },
+    {
+        "question": "كت جيرايا",
+        "response": "جيرايا"
+    },
+    {
+        "question": "كت كوكومي",
+        "response": "كوكومي"
+    },
+    {
+        "question": "كت غيو",
+        "response": "غيو"
+    },
+    {
+        "question": "كت تشيو",
+        "response": "تشيو"
+    },
+    {
+        "question": "كت ساسوري",
+        "response": "ساسوري"
+    },
+    {
+        "question": "كت نيزوكو",
+        "response": "نيزوكو"
+    },
+    {
+        "question": "كت ميدوريا",
+        "response": "ميدوريا"
+    },
+    {
+        "question": "كت نيجي",
+        "response": "نيجي"
+    },
+    {
+        "question": "كت ليزا",
+        "response": "ليزا"
+    },
+    {
+        "question": "كت كاغويا",
+        "response": "كاغويا"
+    },
+    {
+        "question": "كت هينا",
+        "response": "هينا"
+    },
+    {
+        "question": "كت الوكا",
+        "response": "الوكا"
+    },
+    {
+        "question": "كت هيناتا",
+        "response": "هيناتا"
+    },
+    {
+        "question": "كت زينتسو",
+        "response": "زينتسو"
+    },
+    {
+        "question": "كت رينغوكو",
+        "response": "رينغوكو"
+    },
+    {
+        "question": "كت كورتا",
+        "response": "كورتا"
+    },
+    {
+        "question": "كت هيسوكا",
+        "response": "هيسوكا"
+    },
+    {
+        "question": "كت انوس",
+        "response": "انوس"
+    },
+    {
+        "question": "كت بياكوغان",
+        "response": "بياكوغان"
+    },
+    {
+        "question": "كت شوكو",
+        "response": "شوكو"
+    },
+    {
+        "question": "كت ديكو",
+        "response": "ديكو"
+    },
+    {
+        "question": "كت يامي",
+        "response": "يامي"
+    },
+    {
+        "question": "كت فانجانس",
+        "response": "فانجانس"
+    },
+    {
+        "question": "كت يوليوس",
+        "response": "يوليوس"
+    },
+    {
+        "question": "كت تسوموغو",
+        "response": "تسوموغو"
+    },
+    {
+        "question": "كت نانامي",
+        "response": "نانامي"
+    },
+    {
+        "question": "كت ايزن",
+        "response": "ايزن"
+    },
+    {
+        "question": "كت زاراكي",
+        "response": "زاراكي"
+    },
+    {
+        "question": "كت اوراهارا",
+        "response": "اوراهارا"
+    },
+    {
+        "question": "كت اينوي",
+        "response": "اينوي"
+    },
+    {
+        "question": "كت غوهان",
+        "response": "غوهان"
+    },
+    {
+        "question": "كت كيريلين",
+        "response": "كيريلين"
+    },
+    {
+        "question": "كت هيناتسورو",
+        "response": "هيناتسورو"
+    },
+    {
+        "question": "كت ريتشيل",
+        "response": "ريتشيل"
+    }
+]
