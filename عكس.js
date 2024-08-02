3bbb50d8be2edaba048f49f3bafd6bacbb252f91
miku-655 <156179530+miku-655@users.noreply.github.com>
2024-08-02 11:41:59 +0300

    Update

---
+++ عكس.js
@@ -0,0 +1,266 @@
+[
+    {
+        "question": "ويليام",
+        "response": "مايليو"
+    },
+    {
+        "question": "نامي",
+        "response": "يمان"
+    },
+    {
+        "question": "لوفي",
+        "response": "يفول"
+    },
+    {
+        "question": "ايرين",
+        "response": "نيريأ"
+    },
+    {
+        "question": "استا",
+        "response": "اتسأ"
+    },
+    {
+        "question": "همتارو",
+        "response": "وراتمح"
+    },
+    {
+        "question": "الويفيا",
+        "response": "ايفيولا"
+    },
+    {
+        "question": "نويل",
+        "response": "ليو"
+    },
+    {
+        "question": "رايلي",
+        "response": "يليار"
+    },
+    {
+        "question": "ال",
+        "response": "لا"
+    },
+    {
+        "question": "لاو",
+        "response": "ولا"
+    },
+    {
+        "question": "مادارا",
+        "response": "ارادام"
+    },
+    {
+        "question": "يوريتشي",
+        "response": "يستيروي"
+    },
+    {
+        "question": "ايتاتشي",
+        "response": "يچاتئ"
+    },
+    {
+        "question": "توبي",
+        "response": "يبوت"
+    },
+    {
+        "question": "نوبارا",
+        "response": "ارابون"
+    },
+    {
+        "question": "ليفاي",
+        "response": "يالفيل"
+    },
+    {
+        "question": "هاشيراما",
+        "response": "اماراشي"
+    },
+    {
+        "question": "بوروتو",
+        "response": "وتروب"
+    },
+    {
+        "question": "يوسوب",
+        "response": "بوبسو"
+    },
+    {
+        "question": "ريم",
+        "response": "مير"
+    },
+    {
+        "question": "اوتشيها شيسوي",
+        "response": "يسويش اهتيكو"
+    },
+    {
+        "question": "كيد",
+        "response": "ديك"
+    },
+    {
+        "question": "ميكاسا",
+        "response": "اساكيم"
+    },
+    {
+        "question": "دانزو",
+        "response": "وزناد"
+    },
+    {
+        "question": "وينغ",
+        "response": "غنيو"
+    },
+    {
+        "question": "تينشيهان",
+        "response": "نهيشنت"
+    },
+    {
+        "question": "بيكوهان",
+        "response": "نهوكيب"
+    },
+    {
+        "question": "كازيكاجي",
+        "response": "يجاكزك"
+    },
+    {
+        "question": "جيرايا",
+        "response": "اياراج"
+    },
+    {
+        "question": "كازيكاجي",
+        "response": "يجاكزك"
+    },
+    {
+        "question": "كوكومي",
+        "response": "يموكوك"
+    },
+    {
+        "question": "غيو",
+        "response": "ويغ"
+    },
+    {
+        "question": "تشيو",
+        "response": "ويتش"
+    },
+    {
+        "question": "ساسوري",
+        "response": "يروساس"
+    },
+    {
+        "question": "نيزوكو",
+        "response": "وكوزين"
+    },
+    {
+        "question": "ميدوريا",
+        "response": "ايروديم"
+    },
+    {
+        "question": "نيجي",
+        "response": "يجي"
+    },
+    {
+        "question": "استا",
+        "response": "اتسأ"
+    },
+    {
+        "question": "ليزا",
+        "response": "ازي"
+    },
+    {
+        "question": "كاغويا",
+        "response": "اياوغك"
+    },
+    {
+        "question": "هينا",
+        "response": "اناه"
+    },
+    {
+        "question": "الوكا",
+        "response": "اكولا"
+    },
+    {
+        "question": "هيناتا",
+        "response": "اتانهي"
+    },
+    {
+        "question": "زينتسو",
+        "response": "وسنتي"
+    },
+    {
+        "question": "رينغوكو",
+        "response": "وكوغنير"
+    },
+    {
+        "question": "ليفاي",
+        "response": "يالفيل"
+    },
+    {
+        "question": "كورتا",
+        "response": "اترك"
+    },
+    {
+        "question": "هيسوكا",
+        "response": "اكوسيه"
+    },
+    {
+       "question": "انوس",
+        "response": "سون"
+    },
+    {
+       "question": "بياكوغان",
+        "response": "ناغوكاب"
+    },
+    {
+       "question": "شوكو",
+        "response": "وكوش"
+    },
+    {
+       "question": "ديكو",
+        "response": "وكيد"
+    },
+    {
+       "question": "يامي",
+        "response": "يمال"
+    },
+    {
+       "question": "فانجانس",
+        "response": "سنجانف"
+    },
+    {
+       "question": "يوليوس",
+        "response": "سويول"
+    },
+    {
+       "question": "تسوموغو",
+        "response": "وجوموتس"
+    },
+    {
+       "question": "نانامي",
+        "response": "يمنان"
+    },
+    {
+       "question": "ايزن",
+        "response": "نزي"
+    },
+    {
+       "question": "زاراكي",
+        "response": "يكراز"
+    },
+    {
+       "question": "اوراهارا",
+        "response": "اراهرو"
+    },
+    {
+       "question": "اينوي",
+        "response": "يوين"
+    },
+    {
+       "question": "غوهان",
+        "response": "ناهوغ"
+    },
+    {
+       "question": "كيريلين",
+        "response": "نيليرك"
+    },
+    {
+       "question": "هيناتسورو",
+        "response": "وروساتني"
+    },
+    {
+       "question": "ريتشيل",
+       "response": "ليتشير"
+    }
+]
